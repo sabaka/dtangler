27e1885d4064b0545e8f1fc230a4864f58f41fe0
Ilja Dubinin <iliadubinin91@gmail.com>
2016-06-27 23:49:03 +0100

    Issue #2: everything except core has been removed

--- src/dtangler-swingui/dsmgui.bat
+++
@@ -1,1 +0,0 @@
-java -cp lib-forms\forms-1.2.0.jar;dtangler-gui.jar org.dtangler.swingui.app.Main %*
--- src/dtangler-swingui/dsmgui.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -cp lib-forms\forms-1.2.0.jar:dtangler-gui.jar org.dtangler.swingui.app.Main
